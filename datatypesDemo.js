9a361e1c86103f77959f83202e16cfd9381da186
CP-Rahul <rahulcp1401@gmail.com>
2023-08-01 13:46:34 +0530

    Added data types notes

---
+++ datatypesDemo.js
@@ -0,0 +1,24 @@
+var mark = 100;
+var name = "Rahul";
+var isStudent = true;
+var company;
+var position = undefined;
+var salary = null;
+
+console.log("Mark = ", mark);
+console.log("Name = ", name);
+console.log("Is a student = ", isStudent);
+console.log("Company = ", company);
+console.log("Position = ", position);
+console.log("Salary = ", salary);
+
+var user = {
+  mark: 100,
+  name: "Rahul",
+  isStudent: true,
+  company,
+  position: undefined,
+  salary: null,
+};
+
+console.log("User details are : ", user)
