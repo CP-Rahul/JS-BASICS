fd64a4b9776b5b3e7fb830aa6a5dbbbfe9a047c9
CP-Rahul <rahulcp1401@gmail.com>
2023-08-01 14:47:10 +0530

    Added basic operators notes

---
+++ operatorsDemo.js
@@ -0,0 +1,25 @@
+// Arithmetic Operators
+console.log(10 + 5); // Addition
+console.log(10 - 5); // Subtraction
+console.log(10 * 5); // Multiplication
+console.log(10 / 5); // Division -> Gives the quotient
+console.log(10 % 5); // Modulo -> Gives the reminder
+console.log(10 ** 3); // exponent 
+
+// Assignment Operators
+var a = 10; // assigns value in RHS to LHS
+console.log("Value of a is ", a);
+console.log(a += 1); // a = a + 1
+console.log(a -= 1); // a = a - 1
+console.log(a *= 1); // a = a * 1
+console.log(a /= 1); // a = a / 1
+console.log(a %= 1); // a = a % 1
+
+// Ralational Operators
+var x = 10;
+var y = 5;
+console.log("x < y", x < y);
+console.log("x > y", x > y);
+console.log("x <= y", x <= y);
+console.log("x >= y", x >= y);
+
