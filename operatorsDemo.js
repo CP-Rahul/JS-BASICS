ab32af79541b93be334a344ed60c62201bfbb1fd
CP-Rahul <rahulcp1401@gmail.com>
2023-08-02 11:10:46 +0530

    Added special numbers notes

--- operatorsDemo.js
+++ operatorsDemo.js
@@ -1,39 +1,39 @@
-// Arithmetic Operators
-console.log(10 + 5); // Addition
-console.log(10 - 5); // Subtraction
-console.log(10 * 5); // Multiplication
-console.log(10 / 5); // Division -> Gives the quotient
-console.log(10 % 5); // Modulo -> Gives the reminder
-console.log(10 ** 3); // exponent 
+// // Arithmetic Operators
+// console.log(10 + 5); // Addition
+// console.log(10 - 5); // Subtraction
+// console.log(10 * 5); // Multiplication
+// console.log(10 / 5); // Division -> Gives the quotient
+// console.log(10 % 5); // Modulo -> Gives the reminder
+// console.log(10 ** 3); // exponent 
 
-// Assignment Operators
-var a = 10; // assigns value in RHS to LHS
-console.log("Value of a is ", a);
-console.log(a += 1); // a = a + 1
-console.log(a -= 1); // a = a - 1
-console.log(a *= 1); // a = a * 1
-console.log(a /= 1); // a = a / 1
-console.log(a %= 1); // a = a % 1
+// // Assignment Operators
+// var a = 10; // assigns value in RHS to LHS
+// console.log("Value of a is ", a);
+// console.log(a += 1); // a = a + 1
+// console.log(a -= 1); // a = a - 1
+// console.log(a *= 1); // a = a * 1
+// console.log(a /= 1); // a = a / 1
+// console.log(a %= 1); // a = a % 1
 
-// Ralational Operators
-var x = 10;
-var y = 5;
-console.log("x < y", x < y);
-console.log("x > y", x > y);
-console.log("x <= y", x <= y);
-console.log("x >= y", x >= y);
+// // Ralational Operators
+// var x = 10;
+// var y = 5;
+// console.log("x < y", x < y);
+// console.log("x > y", x > y);
+// console.log("x <= y", x <= y);
+// console.log("x >= y", x >= y);
 
-//Logic Operators
-console.log(true && true);
-console.log(true && false);
-console.log(false && true);
-console.log(false && false);
+// //Logic Operators
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
 
-console.log(true || true);
-console.log(true || false);
-console.log(false || true);
-console.log(false || false);
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
 
-console.log(!true);
-console.log(!false);
+// console.log(!true);
+// console.log(!false);
 
